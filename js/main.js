318c7cee7e99bfcff3b69a97e1d6f99d4a36471f
YorkLu <luyang_1994@foxmail.com>
2020-02-28 18:34:37 +0800

    5

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function iphoneDownLoad(){
+    window.location.href='https://itunes.apple.com/app/id462125072';
+}
+function androidDownLoad(){
+    window.location.href='https://b6.market.xiaomi.com/download/AppStore/0b4c144f76c01da44fbc8a387caee00690243d89e/com.kingpoint.gmcchh.apk';
+}
